233f7ec5751b0e9e435212b1ac740f77910a69eb
ThisOneLittleLight <138976529+ThisOneLittleLight@users.noreply.github.com>
2025-11-26 11:34:37 +0100

    Add level progress and label to start menu UI
    Introduced a new LevelView component to the start menu, displaying the player's current level and experience progress. Updated LevelProgressView.gd to handle and update the level label and progress bar dynamically in response to experience and level changes.

--- Scripts/Runtime/Views/LevelProgressView.gd
+++ Scripts/Runtime/Views/LevelProgressView.gd
@@ -2,7 +2,10 @@ extends Control
 class_name LevelProgressView
 
 
+const LEVEL_LABEL_TEXT : String = "Level: "
+
 @export var level_progress_bar : ProgressBar
+@export var level_label : Label
 
 var level_manager : LevelManager
 
@@ -13,19 +16,24 @@ func _ready() -> void:
     level_manager.exp_increased.connect(_on_exp_increased)
     level_manager.lvl_increased.connect(_on_lvl_increased)
 
+    update_level_label()
+
 
 func _on_exp_increased(_new_exp):
     update_level_progress_bar()
 
 
 func _on_lvl_increased(_new_lvl):
-    pass
-    #update_level_progress_bar()
+    update_level_label()
 
 
 func update_level_progress_bar():
     level_progress_bar.max_value = level_manager.experience_for_next_level
     level_progress_bar.value = level_manager.current_experience
+
+
+func update_level_label():
+    level_label.text = LEVEL_LABEL_TEXT + str(level_manager.current_level)
 
 
 func _input(event: InputEvent) -> void:
